b7c9ba5513e7d8c792c60299789dd7232115b8c5
Aaron <awong@jibestream.com>
2018-09-16 14:40:48 -0400

    Initial Commit

---
+++ Benji/Benji.h
@@ -0,0 +1,19 @@
+//
+//  Benji.h
+//  Benji
+//
+//  Created by Aaron Wong-Ellis on 2018-09-16.
+//  Copyright © 2018 kinactiv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Benji.
+FOUNDATION_EXPORT double BenjiVersionNumber;
+
+//! Project version string for Benji.
+FOUNDATION_EXPORT const unsigned char BenjiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Benji/PublicHeader.h>
+
+
