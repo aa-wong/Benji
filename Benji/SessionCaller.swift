3e2cc4896912010646abb6519919a466ff7b4193
Aaron <awong@jibestream.com>
2018-10-13 23:58:49 -0400

    update with support of swift 4,2 for iOS 12

---
+++ Benji/SessionCaller.swift
@@ -0,0 +1,13 @@
+//
+//  SessionCaller.swift
+//  Benji
+//
+//  Created by Aaron Wong-Ellis on 2018-10-13.
+//  Copyright © 2018 kinactiv. All rights reserved.
+//
+
+import UIKit
+
+class SessionCaller: NSObject {
+
+}
